ad3dab2b6c5c8b106f5fe3c530f2e36f70b8f9ba
NSnake151 <19525940+NSnake151@users.noreply.github.com>
2017-09-30 20:00:54 +0800

    this is why we are changing this

--- ICT290-Group-Project/SDL2-devel-2.0.5-VC/CAMPUS TOUR/AABBLinkedList.h
+++ ICT290-Group-Project/SDL2-devel-2.0.5-VC/CAMPUS TOUR/AABBLinkedList.h
@@ -1,6 +1,6 @@
 //  AABBLinkedList.h
 //  Header file for the AABBLinkedList class
-//  Linked List used to store nodes (AABBNode) which contain the co-ordinates of the 
+//  Linked List used to store nodes (AABBNode) which contain the co-ordinates of the
 //  boundings boxes which are used for the collsion detection.
 //
 //	The program splits the world into four quadrants and creates a linked list to
@@ -19,70 +19,70 @@
 #include <freeglut.h>
 
 //--------------------------------------------------------------------------------------
-/**
-@class AABBLinkedList
-@brief Linked list used to store the bounding box nodes
+    /**
+    @class AABBLinkedList
+    @brief Linked list used to store the bounding box nodes
 
-Linked list used to store nodes (AABBNode) which contain the co-ordinates of the bounding
-boxes which are used for the collision detection
+    Linked list used to store nodes (AABBNode) which contain the co-ordinates of the bounding
+    boxes which are used for the collision detection
 
-The program splits the world into four quadrants and creates a linked list to store the
-bounding box details for each.
+    The program splits the world into four quadrants and creates a linked list to store the
+    bounding box details for each.
 
-@author Shay Leary
-@version 01
-@date MAR05
+    @author Shay Leary
+    @version 01
+    @date MAR05
 
-@author Team CodeJunkies
-@version 02
-@date 05SEP17, added doxygen comments
+    @author Team CodeJunkies
+    @version 02
+    @date 05SEP17, added doxygen comments
 
-*/
+    */
 class AABBLinkedList
 {
 public:
-	// constructor creates pointer to first node
+	/// constructor creates pointer to first node
 	AABBLinkedList() { m_first = new AABBNode; }
 
 	virtual ~AABBLinkedList() { Clear(); }
 
 	//----------------------------------------------------------------------------------
 
-	/**
-	@brief Clears the linked list and frees memory
+        /**
+        @brief Clears the linked list and frees memory
 
-	@return void
-	*/
+        @return void
+        */
 	void Clear();
 
-	/**
-	@brief Adds a node to the start of the linked list
+        /**
+        @brief Adds a node to the start of the linked list
 
-	@param maxX - The maximum value on the X axis
-	@param minX - The minimum value on the X axis
-	@param maxY - The maximum value on the Y axis
-	@param minY - The minimum value on the Y axis
-	@param maxZ - The maximum value on the Z axis
-	@param minZ - The minimum value on the Z axis
+        @param maxX - The maximum value on the X axis
+        @param minX - The minimum value on the X axis
+        @param maxY - The maximum value on the Y axis
+        @param minY - The minimum value on the Y axis
+        @param maxZ - The maximum value on the Z axis
+        @param minZ - The minimum value on the Z axis
 
-	@return bool
-	*/
+        @return bool
+        */
 	bool AddToStart(GLdouble maxX, GLdouble minX, GLdouble maxY,
 		GLdouble minY, GLdouble maxZ, GLdouble minZ);
 
-	/**
-	@brief Sets the values of the node
+        /**
+        @brief Sets the values of the node
 
-	@param &ptrCount - The position of the node in the list
-	@param maxX - The maximum value on the X axis
-	@param minX - The minimum value on the X axis
-	@param maxY - The maximum value on the Y axis
-	@param minY - The minimum value on the Y axis
-	@param maxZ - The maximum value on the Z axis
-	@param minZ - The minimum value on the Z axis
+        @param &ptrCount - The position of the node in the list
+        @param maxX - The maximum value on the X axis
+        @param minX - The minimum value on the X axis
+        @param maxY - The maximum value on the Y axis
+        @param minY - The minimum value on the Y axis
+        @param maxZ - The maximum value on the Z axis
+        @param minZ - The minimum value on the Z axis
 
-	@return void
-	*/
+        @return void
+        */
 	void SetData(const int &ptrCount,
 		const GLdouble maxX, const GLdouble minX,
 		const GLdouble maxY, const GLdouble minY,
@@ -92,88 +92,88 @@ public:
 	//  Get Methods
 	//----------------------------------------------------------------------------------
 
-	/**
-	@brief Return the maximum X value of the node
+        /**
+        @brief Return the maximum X value of the node
 
-	@param ptrCount - The position of the node in the list
+        @param ptrCount - The position of the node in the list
 
-	@return GLdouble
-	*/
+        @return GLdouble
+        */
 	GLdouble GetMaxX(int ptrCount);
 
-	/**
-	@brief Return the minimum X value of the node
+        /**
+        @brief Return the minimum X value of the node
 
-	@param ptrCount - The position of the node in the list
+        @param ptrCount - The position of the node in the list
 
-	@return GLdouble
-	*/
+        @return GLdouble
+        */
 	GLdouble GetMinX(int ptrCount);
 
-	/**
-	@brief Return the maximum Y value of the node
+        /**
+        @brief Return the maximum Y value of the node
 
-	@param ptrCount - The position of the node in the list
+        @param ptrCount - The position of the node in the list
 
-	@return GLdouble
-	*/
+        @return GLdouble
+        */
 	GLdouble GetMaxY(int ptrCount);
 
-	/**
-	@brief Return the minimum Y value of the node
+        /**
+        @brief Return the minimum Y value of the node
 
-	@param ptrCount - The position of the node in the list
+        @param ptrCount - The position of the node in the list
 
-	@return GLdouble
-	*/
+        @return GLdouble
+        */
 	GLdouble GetMinY(int ptrCount);
 
-	/**
-	@brief Return the maximum Z value of the node
+        /**
+        @brief Return the maximum Z value of the node
 
-	@param ptrCount - The position of the node in the list
+        @param ptrCount - The position of the node in the list
 
-	@return GLdouble
-	*/
+        @return GLdouble
+        */
 	GLdouble GetMaxZ(int ptrCount);
 
-	/**
-	@brief Return the minimum Z value of the node
+        /**
+        @brief Return the minimum Z value of the node
 
-	@param ptrCount - The position of the node in the list
+        @param ptrCount - The position of the node in the list
 
-	@return GLdouble
-	*/
+        @return GLdouble
+        */
 	GLdouble GetMinZ(int ptrCount);
 
-	/**
-	@brief Return the size of the list
+        /**
+        @brief Return the size of the list
 
-	@return int
-	*/
+        @return int
+        */
 	int GetListSize();
 
-	/**
-	@brief Returns the address of the link to the next node in the list
+        /**
+        @brief Returns the address of the link to the next node in the list
 
-	@return AABBNode
-	*/
+        @return AABBNode
+        */
 	AABBNode *GetNext() const { return m_first->GetNext(); }
 
-	/**
-	@brief Return the address of the link to the first node in the list
+        /**
+        @brief Return the address of the link to the first node in the list
 
-	@return AABBNode
-	*/
+        @return AABBNode
+        */
 	AABBNode *GetFirst() const { return m_first; }
 
 	//--------------------------------------------------------------------------------------
 
 private:
-	// pointer to first node in list
+	/// pointer to first node in list
 	AABBNode *m_first;
 
-	// used to clear memory
+	/// used to clear memory
 	void Delete(AABBNode *before);
 
 	// Privatised copy constructor and assignment operator
--- ICT290-Group-Project/SDL2-devel-2.0.5-VC/CAMPUS TOUR/AABBNode.h
+++ ICT290-Group-Project/SDL2-devel-2.0.5-VC/CAMPUS TOUR/AABBNode.h
@@ -13,21 +13,21 @@
 #include <freeglut.h>
 
 //--------------------------------------------------------------------------------------
-/**
-@class AABBNode
-@brief Stores details for each bounding box used for collision detection.
+    /**
+    @class AABBNode
+    @brief Stores details for each bounding box used for collision detection.
 
-Each node is stored in each link of the AABB Linked List.
+    Each node is stored in each link of the AABB Linked List.
 
-@author Shay Leary
-@version 01
-@date MAR05
+    @author Shay Leary
+    @version 01
+    @date MAR05
 
-@author Team CodeJunkies
-@version 02
-@date 05SEP16
+    @author Team CodeJunkies
+    @version 02
+    @date 05SEP16
 
-*/
+    */
 class AABBNode
 {
 public:
@@ -36,111 +36,111 @@ public:
 
 	//----------------------------------------------------------------------------------
 
-	/**
-	@brief Resets the node data
+        /**
+        @brief Resets the node data
 
-	@return void
-	*/
+        @return void
+        */
 	void  Clear();
 
 	//----------------------------------------------------------------------------------
 	//  Get Methods
 	//----------------------------------------------------------------------------------
 
-	/**
-	@brief Returns the maximum X value for the node
+        /**
+        @brief Returns the maximum X value for the node
 
-	@return GLdouble
-	*/
+        @return GLdouble
+        */
 	GLdouble GetMaxX() { return m_BBox.max.x; }
 
-	/**
-	@brief Returns the minimum X value for the node
+        /**
+        @brief Returns the minimum X value for the node
 
-	@return GLdouble
-	*/
+        @return GLdouble
+        */
 	GLdouble GetMinX() { return m_BBox.min.x; }
 
-	/**
-	@brief Returns the maximum Y value for the node
+        /**
+        @brief Returns the maximum Y value for the node
 
-	@return GLdouble
-	*/
+        @return GLdouble
+        */
 	GLdouble GetMaxY() { return m_BBox.max.y; }
 
-	/**
-	@brief Returns the minimum Y value for the node
+        /**
+        @brief Returns the minimum Y value for the node
 
-	@return GLdouble
-	*/
+        @return GLdouble
+        */
 	GLdouble GetMinY() { return m_BBox.min.y; }
 
-	/**
-	@brief Returns the maximum Z value for the node
+        /**
+        @brief Returns the maximum Z value for the node
 
-	@return GLdouble
-	*/
+        @return GLdouble
+        */
 	GLdouble GetMaxZ() { return m_BBox.max.z; }
 
-	/**
-	@brief Returns the minimum Z value for the node
+        /**
+        @brief Returns the minimum Z value for the node
 
-	@return GLdouble
-	*/
+        @return GLdouble
+        */
 	GLdouble GetMinZ() { return m_BBox.min.z; }
 
-	/**
-	@brief Returns the address of the link to the next node in the list
+        /**
+        @brief Returns the address of the link to the next node in the list
 
-	@return AABBNode
-	*/
+        @return AABBNode
+        */
 	AABBNode *GetNext() const { return m_next; }
 
 	//----------------------------------------------------------------------------------
 	//  Set Methods
 	//----------------------------------------------------------------------------------
 
-	/**
-	@brief Sets the data for the bounding box node
+        /**
+        @brief Sets the data for the bounding box node
 
-	@param maxX - The maximum X value for the node
-	@param minX - The minimum X value for the node
-	@param maxY - The maximum Y value for the node
-	@param minY - The minimum Y value for the node
-	@param maxZ - The maximum Z value for the node
-	@param minZ - The minimum Z value for the node
+        @param maxX - The maximum X value for the node
+        @param minX - The minimum X value for the node
+        @param maxY - The maximum Y value for the node
+        @param minY - The minimum Y value for the node
+        @param maxZ - The maximum Z value for the node
+        @param minZ - The minimum Z value for the node
 
-	@return void
-	*/
+        @return void
+        */
 	void SetData(const GLdouble maxX, const GLdouble minX,
 		const GLdouble maxY, const GLdouble minY,
 		const GLdouble maxZ, const GLdouble minZ);
 
-	/**
-	@brief Sets the address of the link to the next node in the list
+        /**
+        @brief Sets the address of the link to the next node in the list
 
-	@param *next - The address of the link to the next node
+        @param *next - The address of the link to the next node
 
-	@return void
-	*/
+        @return void
+        */
 	void SetNext(AABBNode *next) { m_next = next; }
 
 private:
-	// The address of the next node in the list
+	/// The address of the next node in the list
 	AABBNode *m_next;
 
-	// stores x,y,z co-ordinates
+	/// stores x,y,z co-ordinates
 	struct XYZ
 	{
 		GLdouble x, y, z;
 	};
-	// stores max and min values of co-ordinates
+	/// stores max and min values of co-ordinates
 	struct BoundingBox
 	{
 		XYZ max;
 		XYZ min;
 	};
-	// stores above info
+	/// stores above info
 	BoundingBox m_BBox;
 
 	//----------------------------------------------------------------------------------
