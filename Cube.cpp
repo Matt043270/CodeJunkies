d02de41bbab6287cd0f41c9a1ecc8610becf4aec
charlyn123 <31006812+charlyn123@users.noreply.github.com>
2017-10-09 16:14:31 +0800

    Add files via upload

---
+++ Cube.cpp
@@ -0,0 +1,80 @@
+
+#include "Cube.h"
+#include <GL/glut.h>
+
+    /* Moves the cube 512 pixels every second. When it reaches the other side of the window
+    it goes back to the start.*/
+
+Cube::Cube()
+{
+	cubeX = 0;
+	cubeY = 0;
+	rotAngle = 0;
+	lastFrameTime = 0;
+}
+
+
+    // This method moves the cube
+void Cube::MoveCube()
+{
+	if (lastFrameTime == 0)
+    {
+        lastFrameTime = glutGet(GLUT_ELAPSED_TIME);
+    }
+	int now = glutGet(GLUT_ELAPSED_TIME);
+    int elapsedMilliseconds = now - lastFrameTime;
+    float elapsedTime = elapsedMilliseconds / 1000.0f;
+    lastFrameTime = now;
+	int windowWidth = glutGet(GLUT_WINDOW_WIDTH);
+    cubeX *= 512.0f * elapsedTime;
+    if (cubeX > windowWidth)
+    {
+        cubeX -= windowWidth;
+    }
+
+}
+
+    // Moves (translates) the position of the cube.
+void Cube::TranslateCube(int key)
+{
+    switch (key)
+    {
+		case GLUT_KEY_DOWN:      cubeY -= 5;        break;
+		case GLUT_KEY_UP:      cubeY += 5;        break;
+		case GLUT_KEY_LEFT:      cubeX -= 5;        break;
+		case GLUT_KEY_RIGHT:      cubeX += 5;        break;
+		case GLUT_KEY_F1:	rotAngle += 5;		break;
+
+		default: break;
+    }
+}
+
+
+void Cube::RotateCube()
+{
+	rotAngle += 100;
+}
+
+void Cube::DrawCube()
+{
+	glPushMatrix();
+	glTranslatef(64, 64, 0);
+        // Changes the position of the cube x, y, or z directions.
+	glTranslatef(cubeX, cubeY, 0.0f);
+        // Rotates the cube about the x,y or z axis depending where the '1' is specified in the parameters.
+	glRotatef(rotAngle, 0, 0, 1);
+
+	glBegin(GL_QUADS);
+        // Defines each vertex of the cube.
+        glVertex2f(-64.0,-64.0);
+        glVertex2f(64.0,-64.0);
+        glVertex2f(64.0,64.0);
+        glVertex2f(-64.0,64.0);
+        glVertex2f(-64.0,-64.0);
+        glVertex2f(64.0,-64.0);
+        glVertex2f(64.0,64.0);
+        glVertex2f(-64.0,64.0);
+    glEnd();
+
+	glPopMatrix();
+}
---
+++ Cube.h
@@ -0,0 +1,16 @@
+
+class Cube
+{
+    private:
+        float cubeX;
+        float cubeY;
+        float rotAngle;
+        int lastFrameTime;
+
+    public:
+        Cube();
+        void MoveCube();
+        void RotateCube();
+        void TranslateCube(int key);
+        void DrawCube();
+};
